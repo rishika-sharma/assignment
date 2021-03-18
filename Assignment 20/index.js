df15676fa4e7cea327095fbb9d534a6a4a275041
RS <72138650+rishika-sharma@users.noreply.github.com>
2021-03-18 18:52:30 +0530

    Add files via upload

---
+++ Assignment 20/index.js
@@ -0,0 +1,9 @@
+let firstName = prompt("What is Your First Name ?");
+let lastName = prompt("What is Your Last Name ?");
+let age = prompt("What's Your Age ?");
+console.log("Your full name is " + firstName + " " + lastName);
+console.log("You are " + age + " years old");
+
+
+
+
